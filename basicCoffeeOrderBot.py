ee53ed7208f0b80812a7c60f6b7591394d82a5a7
xVanshx <84569056+xVanshx@users.noreply.github.com>
2021-11-27 15:18:00 +0530

    Update basicCoffeeOrderBot.py

--- basicCoffeeOrderBot.py
+++ basicCoffeeOrderBot.py
@@ -1,6 +1,52 @@
-name = input("What is your name sir\Ma'am \n>")
-print("Hello " + name + " here's our menu.")
-print("=============: MENU :==============")
-print(" Coffee\n Burger\n Pizza\n Noodles" )
-order = input(name + " would you like to order \n >")
-print("Thanks!! for ordering "+ name +" your order " + order + " will be ready soon!")
+import smtplib
+print("Welcome Sir/Mam to our cafe")
+# all variables
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+name = input("\nYour Name Please?\n>")
+items = ["Cappuccino", "Americano", "Espresso", "Latte", "cappuccino", "americano", "espresso", "latte"]
+cap = 10
+amer = 15
+esp = 11
+lat = 9
+print("Okay! " + name + " here is our Menu")
+print("1.Cappuccino - 10$")
+print("2.Americano - 15$")
+print("3.Espresso - 11$")
+print("4.Latte - 9$")
+# main
+while(True):
+  order = input("What you want to order " + name + "?\n>")
+  if order in items[0]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*cap)
+    break
+  elif order in items[4]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*cap)
+    break
+  elif order in items[1]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*amer)
+    break
+  elif order in items[5]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*amer)
+    break
+  elif order in items[2]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*esp)
+    break
+  elif order in items[6]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*esp)
+    break
+  elif order in items[3]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*lat)
+    break
+  elif order in items[7]:
+    quantity = input("How much you want to order?\nEnter no.\n>")
+    print("Your Order " +  order + " will cost you $", int(quantity)*lat)
+    break
+  else:
+    print("Wrong Input Enter Again\n")
